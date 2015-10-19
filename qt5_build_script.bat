040e1e8dfae747adf70025d989b38fc2fafd337c
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-10-19 11:34:33 -0400

    Qt5 and VTK build scripts

--- qt5_build_script.bat
+++ qt5_build_script.bat
@@ -1,23 +1,29 @@
 @ECHO OFF
-set QMAKESPEC=win32-msvc2010
-set QTDIR=F:\Software\Qt5\bld\qtbase
-set PATH=F:\Software\Qt5\bld\bin;F:\Software\Qt5\src\gnuwin32\bin;%PATH%
+
+rem Set up \Microsoft Visual Studio 2015, where <arch> is \c amd64, \c x86, etc.
+rem No need to do this if running from Visual Studio Native Tools x64 command prompt
+rem CALL "C:\Program Files(x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
+set QMAKESPEC=win32-msvc2015
+set QTDIR=C:\Projects\Qt\bld\qtbase
+set PATH=C:\Projects\Qt\bld\bin;C:\Projects\Qt\src\qtrepotools\bin;C:\Projects\Qt\src\gnuwin32\bin;C:\Python27\;"%PROGRAMFILES(x86)%\Git\bin";%PATH%
 
 rem Include libICU for webkit
-set INCLUDE=%INCLUDE%;F:\Software\icu4c-54_1-Win64-msvc10\icu\include
-set LIB=%LIB%;F:\Software\icu4c-54_1-Win64-msvc10\icu\lib64
+rem set INCLUDE=%INCLUDE%;F:\Software\icu4c-54_1-Win64-msvc10\icu\include
+rem set LIB=%LIB%;F:\Software\icu4c-54_1-Win64-msvc10\icu\lib64
 
-rmdir /Q /S F:\Software\Qt5\bld
-mkdir F:\Software\Qt5\bld
-rem cd /D F:\Software\Qt5\src
-rem perl init-repository
-cd /D F:\Software\Qt5\bld
-call ..\src\configure -opensource -confirm-license -debug-and-release -mp -platform win32-msvc2010 -opengl desktop -nomake tests
-nmake
-rem call F:\Software\jom_1_0_14\jom.exe
+rem rmdir /Q /S C:\Projects\Qt\bld
+rem mkdir C:\Projects\Qt\bld
+cd /D C:\Projects\Qt\src
+C:\StrawberryPerl\perl\bin\perl.exe init-repository --no-webkit
+cd /D C:\Projects\Qt\bld
+call ..\src\configure -opensource -confirm-license -debug-and-release -mp -platform win32-msvc2015 -no-icu -opengl desktop -nomake tests -nomake examples
+rem nmake
+call C:\Projects\Qt\jom\jom.exe
+call C:\Projects\Qt\jom\jom.exe install
 rem nmake qdoc3
 rem editbin /STACK:0x200000 bin\qdoc3.exe
 rem nmake docs
-nmake install
-nmake clean
+rem nmake install
+rem nmake clean
 cd ..
--- vtk_build_qt5.bat
+++ vtk_build_qt5.bat
@@ -1,10 +1,21 @@
 @ECHO OFF
 
-SET CMAKE_PREFIX_PATH=F:\Software\Qt5\bld\qtbase\lib\cmake
+SET CURRENT_DIR="%CD%"
+SET CMAKE_PREFIX_PATH=C:\Projects\Qt\bld\qtbase\lib\cmake
+SET PATH=C:\Projects\CMake\install\bin;%PATH%
 
-rmdir /Q /S F:\Projects\vtk\bld-opengl2-qt5-vs10
-mkdir F:\Projects\vtk\bld-opengl2-qt5-vs10
-cd /D F:\Projects\vtk\bld-opengl2-qt5-vs10
+SET VTK_ROOT=C:\Projects\VTK
+SET VTK_SRC=%VTK_ROOT%\src
+SET VTK_BLD=%VTK_ROOT%\bld
 
-call F:\Projects\CMake\install\bin\cmake.exe -DVTK_Group_Qt:BOOL=ON -DVTK_DEBUG_LEAKS:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_EXAMPLES:BOOL=OFF -DVTK_QT_VERSION:STRING=5 -DBUILD_TESTING:BOOL=OFF -DVTK_RENDERING_BACKEND:STRING=OpenGL2 ..\src -G"Visual Studio 10 2010 Win64"
+SET CLEAN_BLD=n
+IF EXIST %VTK_BLD% (SET /p CLEAN_BLD="%VTK_BLD% exists. Clean build tree?[y/N]: ")
+IF %CLEAN_BLD% EQU y (
+	RMDIR /Q /S %VTK_BLD%
+	MKDIR %VTK_BLD%)
+
+cd /D %VTK_BLD%
+
+CALL cmake.exe -DModule_vtkGUISupportQt:BOOL=ON -DVTK_DEBUG_LEAKS:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_EXAMPLES:BOOL=OFF -DVTK_QT_VERSION:STRING=5 -DBUILD_TESTING:BOOL=OFF -DVTK_RENDERING_BACKEND:STRING=OpenGL2 ..\src -G"Visual Studio 14 2015 Win64"
 c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /v:m /p:Configuration=Debug VTK.sln
+cd /D %CURRENT_DIR%
