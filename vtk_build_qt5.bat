02778c9558f984f0395b0b795a50ee0995c043ba
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-11-03 10:28:43 -0500

    Modifications to VTK build script

--- vtk_build_qt5.bat
+++ vtk_build_qt5.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 SET CURRENT_DIR="%CD%"
-SET CMAKE_PREFIX_PATH=C:\Projects\Qt\bld\qtbase\lib\cmake
-SET PATH=C:\Projects\CMake\install\bin;%PATH%
+SET CMAKE_PREFIX_PATH=F:\Software\Qt5\Qt5.5.1\5.5\msvc2013_64\lib\cmake
+SET PATH=F:\Projects\CMake\install\bin;%PATH%
 
-SET VTK_ROOT=C:\Projects\VTK
+SET VTK_ROOT=F:\Projects\vtk
 SET VTK_SRC=%VTK_ROOT%\src
-SET VTK_BLD=%VTK_ROOT%\bld
+SET VTK_BLD=%VTK_ROOT%\bld-opengl2-qt5-vs12
 
 SET CLEAN_BLD=n
 IF EXIST %VTK_BLD% (SET /p CLEAN_BLD="%VTK_BLD% exists. Clean build tree?[y/N]: ")
@@ -16,6 +16,15 @@ IF %CLEAN_BLD% EQU y (
 
 cd /D %VTK_BLD%
 
-CALL cmake.exe -DModule_vtkGUISupportQt:BOOL=ON -DVTK_DEBUG_LEAKS:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_EXAMPLES:BOOL=OFF -DVTK_QT_VERSION:STRING=5 -DBUILD_TESTING:BOOL=OFF -DVTK_RENDERING_BACKEND:STRING=OpenGL2 ..\src -G"Visual Studio 14 2015 Win64"
+CALL cmake.exe -G"Visual Studio 12 2013 Win64" ^
+               -DModule_vtkGUISupportQt:BOOL=ON ^
+               -DVTK_DEBUG_LEAKS:BOOL=ON ^
+               -DBUILD_SHARED_LIBS:BOOL=ON ^
+               -DBUILD_EXAMPLES:BOOL=OFF ^
+               -DVTK_QT_VERSION:STRING=5 ^
+               -DBUILD_TESTING:BOOL=OFF ^
+               -DVTK_RENDERING_BACKEND:STRING=OpenGL2 ^
+               %VTK_SRC%
+
 c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /v:m /p:Configuration=Debug VTK.sln
 cd /D %CURRENT_DIR%
