fe5ff25c5ef5142fd18373233dbcdd6984fe6ded
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2016-02-11 14:55:45 -0500

    CMake build script fetches source if not present

--- cmake_build_script.bat
+++ cmake_build_script.bat
@@ -1,11 +1,23 @@
 SET CURRENT_DIR="%cd%"
-SET CMAKE_ROOT=C:\Projects\CMake
-SET CMAKE_EXE=%CMAKE_ROOT%\cmake-3.2.2-win32-x86\bin\cmake
+
+REM Useful variables that should be set for the script to run
+SET CMAKE_ROOT=D:\Projects\CMake
+SET CMAKE_EXE=D:\Software\cmake-3.5-rc1\bin\cmake
+SET GIT_PATH="%PROGRAMFILES%\Git"
+
 SET CMAKE_SRC=%CMAKE_ROOT%\src
 SET CMAKE_BLD=%CMAKE_ROOT%\bld
 SET CMAKE_INSTALL=%CMAKE_ROOT%\install
 
-SET CMAKE_PREFIX_PATH=C:\Projects\Qt\bld\qtbase\lib\cmake
+SET CMAKE_PREFIX_PATH=D:\Projects\Qt\bld\qtbase\lib\cmake
+
+IF NOT EXIST %CMAKE_SRC% (
+	CALL %GIT_PATH%\bin\git.exe clone git://cmake.org/cmake.git %CMAKE_SRC%
+)
+
+IF NOT EXIST %CMAKE_BLD% (
+	mkdir %CMAKE_BLD%
+)
 
 cd /D %CMAKE_BLD%
 CALL %CMAKE_EXE% -G"Visual Studio 14 2015 Win64" -DBUILD_TESTING:BOOL=OFF -DBUILD_QtDialog:BOOL=ON -DCMAKE_INSTALL_PREFIX:PATH=%CMAKE_INSTALL% %CMAKE_SRC%
