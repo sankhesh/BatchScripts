b54dc3ae96316aed35cbd7f1e5eb7eaa4e945573
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2017-01-27 09:32:41 -0500

    Fix issues with Qt5 build script

--- qt5_build_script.bat
+++ qt5_build_script.bat
@@ -7,21 +7,23 @@ rem Set up \Microsoft Visual Studio 2015, where <arch> is \c amd64, \c x86, etc.
 rem No need to do this if running from Visual Studio Native Tools x64 command prompt
 rem CALL "C:\Program Files(x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-set PYTHON_DIR=D:\Software\Python2.7.11\
-set PERL_DIR=D:\Software\StrawberryPerl5.22.1.2\
-set JOM_DIR=D:\Software\jom_1_1_0\
-set QT_PATH=D:\Projects\Qt
+set PYTHON_DIR=C:\Software\Python27\
+set PERL_DIR=C:\Software\StrawberryPerl\
+set JOM_DIR=C:\Software\jom\
+set QT_PATH=C:\Projects\Qt
 set GIT_PATH="%PROGRAMFILES%\Git"
+set GIT_EXE=%GIT_PATH%\bin\git.exe
 
-set QT_SRC=%QT_PATH%\src
+set QT_SRC=%QT_PATH%\qt5
 set QT_BLD=%QT_PATH%\bld
+set QT_INSTALL=%QT_PATH%\install
 set QTDIR=%QT_BLD%\qtbase
 set QMAKESPEC=win32-msvc2015
-set PATH=%QT_BLD%\bin;%QT_SRC%\qtrepotools\bin;%QT_SRC%\gnuwin32\bin;%PYTHON_DIR%;"%GIT_PATH%\bin";%PATH%
+set PATH=%QT_BLD%\bin;%QTDIR%\bin;%QT_SRC%\qtrepotools\bin;%QT_SRC%\gnuwin32\bin;%PYTHON_DIR%;"%GIT_PATH%\bin";%PERL_DIR%\perl\bin;%PATH%
 
 set SRC_EXISTED=1
 if NOT EXIST %QT_SRC% (
-	CALL %GIT_PATH%\bin\git.exe clone git://code.qt.io/qt/qt5.git %QT_SRC%
+	CALL %GIT_EXE% clone git://code.qt.io/qt/qt5.git %QT_SRC%
 	set SRC_EXISTED=0
 )
 
@@ -38,8 +40,9 @@ if %SRC_EXISTED% GTR 0 (
 
 if %CLEAN_BLD% EQU y (
 	rmdir /Q /S %QT_BLD%
-	mkdir %QT_BLD%
 )
+
+mkdir %QT_BLD%
 
 set INIT=n
 if %SRC_EXISTED% GTR 0 (
@@ -49,20 +52,21 @@ if %SRC_EXISTED% GTR 0 (
 )
 if %INIT% EQU y (
 	cd /D %QT_SRC%
-	%PERL_DIR%\perl\bin\perl.exe init-repository ^
-	--module-subset=all,-qt3d,-qtactiveqt,-qtandroidextras,-qtcanvas3d,-qtconnectivity,-qtdoc,-qtdocgallery,-qtfeedback,-qtlocation,-qtmacextras,-qtpim,-qtqa,-qtscript,-qtsensors,-qttranslations,-qtwebengine,-qtwebkit,-qtwinextras,-qtx11extras
+	%PERL_DIR%\perl\bin\perl.exe init-repository --module-subset=default,-qt3d,-qtactiveqt,-qtandroidextras,-qtcanvas3d,-qtconnectivity,-qtdoc,-qtdocgallery,-qtfeedback,-qtlocation,-qtmacextras,-qtpim,-qtqa,-qtscript,-qtsensors,-qttranslations,-qtwebengine,-qtwebkit,-qtx11extras,-qtcharts,-qtcharts,-qtwayland,-qtserialbus,-qtscxml,-qtnetworkauth,-qtdatavis3d,-qtgamepad,-qtpurchasing,-qtwebchannel,-qtserialport,-qtspeech,-qtgraphicaleffects,-qtvirtualkeyboard,-qtmultimedia,-qtimageformats
 )
 
 cd /D %QT_SRC%
 REM Get currently checked out tag
 for /f "tokens=1" %%i in ('git describe') do set TAG=%%i
 set /p TAG="TAG to build [%TAG%]: "
-call %GIT_PATH%\bin\git.exe checkout %TAG%
+call %GIT_EXE% checkout %TAG%
+call %GIT_EXE% submodule update
 
 cd /D %QT_BLD%
-call ..\src\configure -opensource -confirm-license -debug-and-release ^
+call %QT_SRC%\configure -opensource -confirm-license -debug-and-release ^
     -mp -platform win32-msvc2015 -no-icu -opengl desktop ^
 	-nomake tests -nomake examples ^
+	-prefix %QT_INSTALL% ^
 	-skip qt3d ^
 	-skip qtactiveqt ^
 	-skip qtandroidextras ^
@@ -80,11 +84,24 @@ call ..\src\configure -opensource -confirm-license -debug-and-release ^
 	-skip qttranslations ^
 	-skip qtwebengine ^
 	-skip qtwebkit ^
-	-skip qtwinextras ^
-	-skip qtx11extras
+	-skip qtx11extras ^
+	-skip qtcharts ^
+	-skip qtwayland ^
+	-skip qtserialbus ^
+	-skip qtserialport ^
+	-skip qtscxml ^
+	-skip qtdatavis3d ^
+	-skip qtgamepad ^
+	-skip qtpurchasing ^
+	-skip qtwebchannel ^
+	-skip qtspeech ^
+	-skip qtgraphicaleffects ^
+	-skip qtvirtualkeyboard ^
+	-skip qtmultimedia ^
+	-skip qtimageformats
 
 call %JOM_DIR%\jom.exe
-call %JOM_DIR%\jom.exe install
+rem call %JOM_DIR%\jom.exe install
 
 rem nmake
 rem nmake qdoc3
