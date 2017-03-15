cb02c29e4a0b226e30248fc7f9f029576ba57557
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2017-03-15 15:02:12 -0400

    Change default Visual studio to 2017

--- qt5_build_script.bat
+++ qt5_build_script.bat
@@ -7,10 +7,10 @@ rem Set up \Microsoft Visual Studio 2015, where <arch> is \c amd64, \c x86, etc.
 rem No need to do this if running from Visual Studio Native Tools x64 command prompt
 rem CALL "C:\Program Files(x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-set PYTHON_DIR=C:\Software\Python27\
-set PERL_DIR=C:\Software\StrawberryPerl\
-set JOM_DIR=C:\Software\jom\
-set QT_PATH=C:\Projects\Qt
+set PYTHON_DIR=D:\Software\Python27\
+set PERL_DIR=D:\Software\StrawberryPerl\
+set JOM_DIR=D:\Software\jom\
+set QT_PATH=D:\Projects\Qt
 set GIT_PATH="%PROGRAMFILES%\Git"
 set GIT_EXE=%GIT_PATH%\bin\git.exe
 
@@ -71,8 +71,8 @@ if %INIT% EQU y (
 )
 
 cd /D %QT_BLD%
-call %QT_SRC%\configure -opensource -confirm-license -debug-and-release ^
-    -mp -platform win32-msvc2015 -no-icu -opengl desktop ^
+call %QT_SRC%\configure -opensource -confirm-license -release ^
+    -mp -platform win32-msvc2017 -no-icu -opengl desktop -verbose^
 	-nomake tests -nomake examples ^
 	-prefix %QT_INSTALL% ^
 	-skip qt3d ^
