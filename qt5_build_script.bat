332b3021a3b198d727a4185e6c89356f7c870cb5
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2017-08-09 15:38:49 -0400

    Unset QMAKESPEC and remove whitespace issues

--- qt5_build_script.bat
+++ qt5_build_script.bat
@@ -19,13 +19,13 @@ set QT_SRC=%QT_PATH%\qt5
 set QT_BLD=%QT_PATH%\bld
 set QT_INSTALL=%QT_PATH%\install
 set QTDIR=%QT_BLD%\qtbase
-set QMAKESPEC=win32-msvc2015
+REM set QMAKESPEC=win32-msvc2015
 set PATH=%QT_BLD%\bin;%QTDIR%\bin;%QT_SRC%\qtrepotools\bin;%QT_SRC%\gnuwin32\bin;%PYTHON_DIR%;"%GIT_PATH%\bin";%PERL_DIR%\perl\bin;%PATH%
 
 set SRC_EXISTED=1
 if NOT EXIST %QT_SRC% (
-	CALL %GIT_EXE% clone git://code.qt.io/qt/qt5.git %QT_SRC%
-	set SRC_EXISTED=0
+  CALL %GIT_EXE% clone git://code.qt.io/qt/qt5.git %QT_SRC%
+  set SRC_EXISTED=0
 )
 
 rem Include libICU for webkit
@@ -34,13 +34,13 @@ rem set LIB=%LIB%;F:\Software\icu4c-54_1-Win64-msvc10\icu\lib64
 
 set CLEAN_BLD=n
 if %SRC_EXISTED% GTR 0 (
-	if EXIST %QT_BLD% (SET /p CLEAN_BLD="%QT_BLD% exists. Clean build tree?[y/N]: ")
+  if EXIST %QT_BLD% (SET /p CLEAN_BLD="%QT_BLD% exists. Clean build tree?[y/N]: ")
 ) else (
-	set CLEAN_BLD=y
+  set CLEAN_BLD=y
 )
 
 if %CLEAN_BLD% EQU y (
-	rmdir /Q /S %QT_BLD%
+  rmdir /Q /S %QT_BLD%
 )
 mkdir %QT_BLD%
 
@@ -74,42 +74,42 @@ if %INIT% EQU y (
 
 cd /D %QT_BLD%
 call %QT_SRC%\configure -opensource -confirm-license -release ^
-    -mp -platform win32-msvc2017 -no-icu -opengl desktop -verbose^
-	-nomake tests -nomake examples ^
-	-prefix %QT_INSTALL% ^
-	-skip qt3d ^
-	-skip qtactiveqt ^
-	-skip qtandroidextras ^
-	-skip qtcanvas3d ^
-	-skip qtcharts ^
-	-skip qtconnectivity ^
-	-skip qtdoc ^
-	-skip qtdocgallery ^
-	-skip qtfeedback ^
-	-skip qtlocation ^
-	-skip qtmacextras ^
-	-skip qtpim ^
-	-skip qtqa ^
-	-skip qtscript ^
-	-skip qtsensors ^
-	-skip qttranslations ^
-	-skip qtwebengine ^
-	-skip qtwebkit ^
-	-skip qtx11extras ^
-	-skip qtcharts ^
-	-skip qtwayland ^
-	-skip qtserialbus ^
-	-skip qtserialport ^
-	-skip qtscxml ^
-	-skip qtdatavis3d ^
-	-skip qtgamepad ^
-	-skip qtpurchasing ^
-	-skip qtwebchannel ^
-	-skip qtspeech ^
-	-skip qtgraphicaleffects ^
-	-skip qtvirtualkeyboard ^
-	-skip qtmultimedia ^
-	-skip qtimageformats
+  -mp -platform win32-msvc2015 -no-icu -opengl desktop -verbose^
+  -nomake tests -nomake examples ^
+  -prefix %QT_INSTALL% ^
+  -skip qt3d ^
+  -skip qtactiveqt ^
+  -skip qtandroidextras ^
+  -skip qtcanvas3d ^
+  -skip qtcharts ^
+  -skip qtconnectivity ^
+  -skip qtdoc ^
+  -skip qtdocgallery ^
+  -skip qtfeedback ^
+  -skip qtlocation ^
+  -skip qtmacextras ^
+  -skip qtpim ^
+  -skip qtqa ^
+  -skip qtscript ^
+  -skip qtsensors ^
+  -skip qttranslations ^
+  -skip qtwebengine ^
+  -skip qtwebkit ^
+  -skip qtx11extras ^
+  -skip qtcharts ^
+  -skip qtwayland ^
+  -skip qtserialbus ^
+  -skip qtserialport ^
+  -skip qtscxml ^
+  -skip qtdatavis3d ^
+  -skip qtgamepad ^
+  -skip qtpurchasing ^
+  -skip qtwebchannel ^
+  -skip qtspeech ^
+  -skip qtgraphicaleffects ^
+  -skip qtvirtualkeyboard ^
+  -skip qtmultimedia ^
+  -skip qtimageformats
 
 call %JOM_DIR%\jom.exe
 call %JOM_DIR%\jom.exe install
