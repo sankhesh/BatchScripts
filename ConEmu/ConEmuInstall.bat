7aa210f2b8fbe88d827c3a19153fa197334aa7b9
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-03-07 12:34:33 -0500

    Update to clink 1.3.9 and auto-install to ConEmu

--- ConEmu/ConEmuInstall.bat
+++ ConEmu/ConEmuInstall.bat
@@ -1,1 +1,4 @@
 powershell -NoProfile -ExecutionPolicy  RemoteSigned -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; "set ver 'alpha'; set dst 'C:\Software\ConEmu'; set lnk $FALSE; set xml 'https://raw.githubusercontent.com/sankhesh/BatchScripts/master/ConEmu/ConEmu.xml'; iex ((new-object net.webclient).DownloadString('https://conemu.github.io/install2.ps1'))"
+
+REM Install Clink
+powershell -NoProfile -ExecutionPolicy RemoteSigned -File %0\..\ClinkInstall.ps1 -dst C:\Software\ConEmu\ConEmu
