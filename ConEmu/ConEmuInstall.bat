9c34064eadfe89903938198d8697f2ab2f3550a2
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-03-30 13:11:10 -0400

    Add symlink instructions for ConEmu configuration

--- ConEmu/ConEmuInstall.bat
+++ ConEmu/ConEmuInstall.bat
@@ -1,3 +1,8 @@
+@ECHO off
 call SoftwareDrive.bat
 set dst=%DRIVETOUSE%:\Software\ConEmu
 start /b /wait powershell -NoProfile -ExecutionPolicy  RemoteSigned -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; "set ver 'alpha'; set dst $env:dst; set lnk $FALSE; set run $FALSE; set xml 'https://raw.githubusercontent.com/sankhesh/BatchScripts/master/ConEmu/ConEmu.xml'; iex ((new-object net.webclient).DownloadString('https://conemu.github.io/install2.ps1'))"
+set conf=%DRIVETOUSE%:\Software\ConEmu\ConEmu.xml
+set mypath=%~dp0
+set lntarget=%mypath%ConEmu.xml
+echo Setup the symlink manually using: "del /q %conf% && mklink %conf% %lntarget%"
