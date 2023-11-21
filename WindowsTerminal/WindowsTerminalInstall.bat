2127a17a62e12256f32cbed482bae24f1a2bb421
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2023-11-21 17:07:05 -0500

    WindowsTerminal: Allow creation of a default settings before symlinking

--- WindowsTerminal/WindowsTerminalInstall.bat
+++ WindowsTerminal/WindowsTerminalInstall.bat
@@ -12,6 +12,11 @@ set dlurl
 rem Download the latest release
 start /b /wait powershell -NoProfile -ExecutionPolicy RemoteSigned -File %0\..\..\ps_scripts\DownloadRelease.ps1 -dst %dst% -url %dlurl% -includes "x64"
 
+start /wait %dst%\wt.exe|rem
+:loop
+tasklist /fi "imagename eq WindowsTerminal.exe" > nul
+if errorlevel 1 goto loop
+taskkill /f /im WindowsTerminal.exe
 rem Add a symlink for settings
 set setFile="%LOCALAPPDATA%\Microsoft\Windows Terminal\settings.json"
 del /q %setFile%
