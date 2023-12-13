d1bd5cd3b86746e6ef04e62caf64da065ffcd09e
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2023-12-13 16:29:44 -0500

    winterm: Make install script wait for default settings creation

--- WindowsTerminal/WindowsTerminalInstall.bat
+++ WindowsTerminal/WindowsTerminalInstall.bat
@@ -1,4 +1,5 @@
-@ECHO off
+rem  @ECHO off
+setlocal enableextensions enabledelayedexpansion
 call SoftwareDrive.bat
 set dst=%DRIVETOUSE%:\Software\WindowsTerminal\
 if NOT EXIST %dst% (
@@ -12,12 +13,14 @@ set dlurl
 rem Download the latest release
 start /b /wait powershell -NoProfile -ExecutionPolicy RemoteSigned -File %0\..\..\ps_scripts\DownloadRelease.ps1 -dst %dst% -url %dlurl% -includes "x64"
 
-start /wait %dst%\wt.exe|rem
+start /wait %dst%\wt.exe
+rem Give time for the process to start
+timeout /t 3 /nobreak > nul
 :loop
-tasklist /fi "imagename eq WindowsTerminal.exe" > nul
-if errorlevel 1 goto loop
+tasklist.exe /fi "imagename eq WindowsTerminal.exe" | find /v "No tasks" > nul && (echo "Default settings created.") || (goto loop)
 taskkill /f /im WindowsTerminal.exe
 rem Add a symlink for settings
 set setFile="%LOCALAPPDATA%\Microsoft\Windows Terminal\settings.json"
 del /q %setFile%
 mklink %setFile% %~dp0\WindowsTerminalSettings.json
+echo "If fonts don't look good, run the InstallCascadiaFonts.bat script!"
